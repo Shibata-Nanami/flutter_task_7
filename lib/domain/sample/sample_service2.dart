061a0d85aa9d0c2a2fa069d06b5330ba49a55f3d
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-27 15:15:43 +0900

    sample_service2.dartファイル作成

---
+++ lib/domain/sample/sample_service2.dart
@@ -0,0 +1,44 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:layer_architecture_template/infrastructure/data_source/isar/todo/todo_collection.dart';
+import 'package:layer_architecture_template/infrastructure/model/sample/sample_model.dart';
+import 'package:layer_architecture_template/infrastructure/repository/sample/sample_repository2.dart';
+
+final sampleService = Provider.autoDispose(
+  (ref) => SampleService2(
+    sampleRepository: ref.read(sampleRepository),
+  ),
+);
+
+class SampleService2 {
+  SampleService2({required this.sampleRepository});
+  final SampleRepository2 sampleRepository;
+
+  Future<List<SampleModel>> fetchSampleModelList() async {
+    return sampleRepository.fetchSampleModelList();
+  }
+
+  ///Isar: 一覧データ取得
+  Future<List<Todo>> fetchTodoList() async {
+    return sampleRepository.fetchTodoList();
+  }
+
+  ///Isar: 単体データ取得
+  Future<Todo?> fetchTodoById(int id) async {
+    return sampleRepository.fetchTodoById(id);
+  }
+
+  ///Isar: データ追加 / 更新
+  Future<void> setTodoData({
+    required Todo todoModel,
+  }) async {
+    return sampleRepository.setTodoData(todoModel: todoModel);
+  }
+
+  ///Isar: データ削除
+  Future<void> deleteTodoData({
+    required int id,
+  }) async {
+    return sampleRepository.deleteTodoData(id: id);
+  }
+}
