6d2b4fe14fdcc78522ca127daf9d33387c1f59af
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-27 19:39:02 +0900

    todo_repository.dartファイル作成

---
+++ lib/infrastructure/repository/todo/todo_repository.dart
@@ -0,0 +1,23 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../data_source/sample/sample_remote_data_source.dart';
+import '../../model/sample/sample_model.dart';
+
+final todoRepository = Provider(
+  (ref) => TodoRepository(
+    sampleDataSource: SampleDataSource(),
+  ),
+);
+
+class TodoRepository {
+  TodoRepository({
+    required this.sampleDataSource,
+  });
+
+  final SampleDataSource sampleDataSource;
+
+//一覧データ取得
+  Future<List<SampleModel>> fetchSampleModelList() async {
+    return sampleDataSource.fetchSampleModelList();
+  }
+}
