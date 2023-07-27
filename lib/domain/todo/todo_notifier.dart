3be5871b408a2f96815936c376d6ecabeff14535
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-27 19:36:37 +0900

    todo_notifierファイル作成

---
+++ lib/domain/todo/todo_notifier.dart
@@ -0,0 +1,32 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:layer_architecture_template/domain/todo/todo_service.dart';
+import 'package:layer_architecture_template/domain/todo/todo_state.dart';
+
+import '../../infrastructure/model/sample/sample_model.dart';
+
+final todoNotifierProvider =
+    StateNotifierProvider.autoDispose<TodoNotifier, TodoState>((ref) {
+  return TodoNotifier(
+    todoService: ref.read(todoService),
+  )..init();
+});
+
+class TodoNotifier extends StateNotifier<TodoState> {
+  TodoNotifier({
+    required this.todoService,
+  }) : super(const TodoState());
+
+  final TodoService todoService;
+
+  Future<void> init() async {
+    // 通常のFutureのサンプル
+    final todoList = await fetchSampleModelList();
+    state = state.copyWith(
+      futureTodoList: AsyncValue.data(todoList),
+    );
+  }
+
+  Future<List<SampleModel>> fetchSampleModelList() async {
+    return todoService.fetchSampleModelList();
+  }
+}
