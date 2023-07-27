a81d4801ce66f8825209c8e79b331980d6472682
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-27 19:36:58 +0900

    todo_state.dartファイル作成

---
+++ lib/domain/todo/todo_state.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../infrastructure/model/sample/sample_model.dart';
+
+part 'todo_state.freezed.dart';
+
+@freezed
+class TodoState with _$TodoState {
+  const factory TodoState({
+    // こちらの場合、state.futureTodoList.when~が使える
+    @Default(AsyncValue.loading()) AsyncValue<List<SampleModel>> futureTodoList,
+  }) = _TodoState;
+}
