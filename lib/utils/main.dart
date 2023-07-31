93fe82df715bd100d819770a5db739a9306831e7
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-31 10:06:48 +0900

    レビュー修正

--- lib/utils/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_core/firebase_core.dart';
-
-import '../firebase_options.dart';
-
-Future<void> main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-}
