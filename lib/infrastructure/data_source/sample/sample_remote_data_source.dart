ee1a0e3694da83f14a70fd10604f40df866e4214
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-31 15:07:36 +0900

    firebaseからデータを取ってくるコードに修正

--- lib/infrastructure/data_source/sample/sample_remote_data_source.dart
+++ lib/infrastructure/data_source/sample/sample_remote_data_source.dart
@@ -1,30 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../model/sample/sample_model.dart';
 
 class SampleDataSource {
-  /// 3秒遅延させて、ダミーのデータを返す
+  final List<SampleModel> _list = [];
+  List<SampleModel> get list => _list;
+  bool isLoading = false;
+
   Future<List<SampleModel>> fetchSampleModelList() async {
-    await Future.delayed(const Duration(seconds: 3));
-    return [
-      const SampleModel(
-        id: '1',
-        name: 'サンプルラベル1',
-      ),
-      const SampleModel(
-        id: '2',
-        name: 'サンプルラベル2',
-      ),
-      const SampleModel(
-        id: '3',
-        name: 'サンプルラベル3',
-      ),
-      const SampleModel(
-        id: '4',
-        name: 'サンプルラベル4',
-      ),
-      const SampleModel(
-        id: '5',
-        name: 'サンプルラベル5',
-      ),
-    ];
+    // Firestoreのコレクション参照を取得
+    final collectionRef = FirebaseFirestore.instance.collection('memo');
+
+    // Firestoreのドキュメント一覧を取得
+    final snapshot = await collectionRef.get();
+
+    // Firestoreのドキュメント一覧からSampleModelリストを作成
+    final memoList = snapshot.docs.map((doc) {
+      //各ドキュメントのデータ（フィールドとその値）を取得し、dataに代入
+      final data = doc.data();
+      return SampleModel(id: doc.id, name: data['name'] ?? '');
+    }).toList();
+    return memoList;
   }
 }
