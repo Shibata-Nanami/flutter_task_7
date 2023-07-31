9e9f0af82f2d237c51e08583643fac393bbfbcd6
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-31 14:36:08 +0900

    firebase初期化処理の設定

--- lib/initialize.dart
+++ lib/initialize.dart
@@ -1,17 +1,12 @@
 import 'dart:async';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:layer_architecture_template/isar_instance.dart';
-
 import 'firebase_options.dart';
 
 class Initialize {
   Future<void> init() async {
     // アプリ実行前の初期化処理
     await IsarInstance().init();
-  }
-
-  Future<void> main() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
